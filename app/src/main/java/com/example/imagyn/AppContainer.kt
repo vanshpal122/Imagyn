acbec7e63f763d705aaea93d4c9a6d2731448ee2
VANSH PAL <147159987+vanshpal122@users.noreply.github.com>
2025-06-01 20:04:35 +0530

    Added Helpful Comments

--- app/src/main/java/com/example/imagyn/AppContainer.kt
+++ app/src/main/java/com/example/imagyn/AppContainer.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import com.example.imagyn.data.ImagynRepository
 import com.example.imagyn.data.database.ImagynDatabase
 
+//Interface that specifies what dependencies are needed throughout the app
 interface AppContainer {
     val imagynRepository: ImagynRepository
 }
 
+//Actual Instantiation of the dependencies 
 class AppDataContainer(context: Context): AppContainer {
     override val imagynRepository: ImagynRepository by lazy {
         ImagynRepository(ImagynDatabase.getDatabase(context).cardDao())
