a7ebc79ec4aa2dd349d5797ba37dbb8705209061
vanshpal <vanshpal921@gmail.com>
2025-07-06 12:37:30 +0530

    Updated plugins and SDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.imagyn"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.imagyn"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
